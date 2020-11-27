114776e087af130c3d4a9d4e24540cf918abf66b
huangzijian <huangzijian@lamdaer.com>
2020-11-28 01:56:25 +0800

    feat(payment8001): 支付微服务 8001 入驻 Eureka

--- cloud-provider-payment8001/src/main/java/com/lamdaer/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/lamdaer/springcloud/PaymentMain8001.java
@@ -2,12 +2,14 @@ package com.lamdaer.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lamdaer
  * @createTime 2020/10/28
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
