b9bc8c785dcc7d17cb0e5b78f4b5eaf553cb254a
huangzijian <huangzijian@lamdaer.com>
2020-11-28 02:03:26 +0800

    feat(order80): 订单微服务 80 入驻 Eureka

--- cloud-consumer-order80/src/main/java/com/lamdaer/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/lamdaer/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.lamdaer.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author lamdaer
  * @createTime 2020/11/2
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
